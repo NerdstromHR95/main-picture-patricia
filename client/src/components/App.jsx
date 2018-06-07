3846d07f9aada129f5b3766f81a37b43cddc42c6
Papatiara <shirazipatricia@gmail.com>
2018-06-07 16:00:35 -0700

    Carousel working and changing main photo

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,32 +1,99 @@
 
 import React from 'react';
+import axios from 'axios';
+import $ from 'jquery';
 import '../main.css';
-import images from './dummydata';
-import Carousel from './Carousel';
+import images from './firstImages';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: images,
-      mainImage: images[0].main,
+      products: images.polo,
+      mainImage: images.polo[0],
+      click: false,
     };
+    this.get = this.get.bind(this);
+  }
+  componentDidMount() {
+    this.get();
+  }
+  get() {
+    axios.get('/home')
+      .then((response) => {
+        this.setState({
+          products: response.data,
+        });
+      });
   }
 
-  changeMainImage(img) {
-    this.setState = {
-      mainImage: img,
-    };
+  slideUp(hidden) {
+    if (this.state.click === false) {
+      $('#container-left').slideUp('slow');
+      $('#slide').append(`<div id="container-left" key={index}> <img src= ${hidden} alt="thumbnails" /> </div>`).fadeIn('slow').addClass('1');
+      $('#topArrow').remove();
+      $('#top').prepend($('<input id="downArrow" type="image" src="https://image.ibb.co/fQGP58/Screen_Shot_2018_06_05_at_11_40_00.png" alt="arrow" />'));
+      this.setState({
+        click: true,
+      });
+    }
   }
 
+  changeMainPhoto(event) {
+    const clicked = event.target.src;
+    this.setState({
+      mainImage: clicked,
+    });
+  }
   render() {
+    $('#downArrow').on('click', () => {
+      $('#downArrow').remove();
+      $('#container-left').slideDown('slow');
+      $('.1').remove();
+      $('#slide').prepend($('<input id="topArrow" type="image" src="https://image.ibb.co/fQGP58/Screen_Shot_2018_06_05_at_11_40_00.png" alt="arrow" />'));
+      this.setState({
+        click: false,
+      });
+    });
+    $('#topArrow').on('click', (event) => {
+      event.preventDefault();
+      this.slideUp();
+    });
+    const thumbnails = [];
+    const allImages = [];
+    let extras = [];
+    const hidden = [];
+    for (let i = 0; i < this.state.products.length; i += 1) {
+      allImages.push(<div id="allImages"> <img src={this.state.products.length[i]} alt="thumbnails" /> </div>);
+    }
+    extras = this.state.products.slice(5, this.state.products.length + 1);
+    for (let i = 0; i < extras.length; i += 1) {
+      hidden.push(extras[i]);
+    }
+    if (this.state.products.length >= 5) {
+      for (let i = 0; i < 5; i += 1) {
+        thumbnails.push(this.state.products[i]);
+      }
+      return (
+        <div>
+          <div id="container">
+            <img src={this.state.mainImage} alt="test" />
+          </div>
+          <div id="top" />
+          {thumbnails.map((img, index) =>
+            <div onClick={(event => this.changeMainPhoto(event))} id="container-left" key={index}> <img src={img} alt="thumbnails" /> </div>)}
+          <div id="slide" />
+          <input onClick={() => this.slideUp(hidden)} id="topArrow" type="image" src="https://image.ibb.co/fQGP58/Screen_Shot_2018_06_05_at_11_40_00.png" alt="arrow" />
+        </div>
+      );
+    }
     return (
       <div>
         <div id="container">
           <img src={this.state.mainImage} alt="test" />
         </div>
         {this.state.products.map((img, index) =>
-          <Carousel func={this.changeMainImage} img={img} key={index} />)}
+          <div id="container-left" key={index}> <img src={img} alt="thumbnails" /> </div>)}
       </div>
     );
   }
--- schema.sql
+++ schema.sql
@@ -15,9 +15,12 @@ CREATE TABLE products (
 );
 
 
-INSERT INTO products (ID , product_id, product_type, picture_type, color, img_url) VALUES (0, 100, 'polo', 'front', 'white', 'https://ibb.co/bsAWsd');
-INSERT INTO products (ID , product_id, product_type, picture_type, color, img_url) VALUES (1, 102, 'polo', 'back', 'black', 'https://ibb.co/bsAWsd');
-INSERT INTO products (ID , product_id, product_type, picture_type, color, img_url) VALUES (3, 103, 'polo', 'side', 'black', 'https://ibb.co/bsAWsd');
+INSERT INTO products (ID , product_id, product_type, picture_type, color, img_url) VALUES (0, 100, 'polo', 'front', 'white', 'https://n.nordstrommedia.com/ImageGallery/store/product/Zoom/16/_12283756.jpg?crop=pad&pad_color=FFF&format=jpeg&w=1660&h=2547');
+INSERT INTO products (ID , product_id, product_type, picture_type, color, img_url) VALUES (1, 100, 'polo', 'back', 'white', 'https://n.nordstrommedia.com/ImageGallery/store/product/Zoom/11/_12283791.jpg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1197');
+INSERT INTO products (ID , product_id, product_type, picture_type, color, img_url) VALUES (2, 100, 'polo', 'side', 'white', 'https://n.nordstrommedia.com/ImageGallery/store/product/Zoom/13/_12283813.jpg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1197');
+INSERT INTO products (ID , product_id, product_type, picture_type, color, img_url) VALUES (3, 100, 'polo', 'collar', 'white', 'https://n.nordstrommedia.com/ImageGallery/store/product/Zoom/17/_12283777.jpg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1197');
+INSERT INTO products (ID , product_id, product_type, picture_type, color, img_url) VALUES (4, 100, 'polo', 'fabrica', 'white', 'https://n.nordstrommedia.com/ImageGallery/store/product/Zoom/8/_12272768.jpg?crop=pad&pad_color=FFF&format=jpeg&w=780&h=1197');
+INSERT INTO products (ID , product_id, product_type, picture_type, color, img_url) VALUES (5, 100, 'polo', 'product', 'white', 'https://n.nordstrommedia.com/ImageGallery/store/product/Zoom/5/_101841105.jpg?crop=pad&pad_color=FFF&format=jpeg&w=60&h=90');
 
 
 
